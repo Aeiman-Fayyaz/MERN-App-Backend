ae7feaf3776100b08bcaa64907424d2e4d076f2f
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2026-05-08 23:45:01 +0500

    update

--- server.js
+++ server.js
@@ -18,4 +18,3 @@ app.use("/api/auth", authRoutes)
 app.get("/", (req, res) => res.send("API is running..."))
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-module.exports = app
