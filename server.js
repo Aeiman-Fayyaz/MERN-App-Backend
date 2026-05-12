df25a899161529b4de72dacebe829bd83bde3130
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2026-05-13 00:27:41 +0500

    Change export syntax from ES6 to CommonJS

--- server.js
+++ server.js
@@ -36,4 +36,6 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-export default app
+// export default app
+
+module.exports = app;
