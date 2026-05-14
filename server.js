dcae1a85d9ecbd2dadb11650441f6271fd133ddc
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2026-05-14 22:47:08 +0500

    Switch from ES6 to CommonJS module export
    Change export method for compatibility with Vercel.

--- server.js
+++ server.js
@@ -36,4 +36,5 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-export default app
+// export default app
+module.exports = app; // Vercel ke liye zaroori hai
